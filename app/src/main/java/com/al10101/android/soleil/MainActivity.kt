cb0a425e63ec3d1be338957bd1171ab9700290ee
Alberto Cabrera <albertocabja@gmail.com>
2022-12-20 20:03:57 -0600

    Initialized Android Project

---
+++ app/src/main/java/com/al10101/android/soleil/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.al10101.android.soleil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
